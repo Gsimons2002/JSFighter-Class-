e2af21cffc808d848f92d4123fbfc2cc05263d1e
jonathan Torres <jonat2021888@live.ytech.edu>
2019-11-21 14:19:21 -0500

    Update index.js
    My code broke so i re-uesd it?

--- script/index.js
+++ script/index.js
@@ -3,25 +3,20 @@ const START_SP = 10;
 const DEFAULT_ATK = 5;
 const DEFAULT_DEF = 5;
 const DEFAULT_TEK = 5;
-
 //sets constants names
 const P0NAME = 'Crash'
 const P0CHARA = 'crashr'
 const P1NAME = 'Sam'
 const P1CHARA = 'saml'
-
 let playerTurn = false;
 let logging = true;
-
 let Player0;
 let Player1;
-
 //varible for finding % of players HP used for the HealthBar
 let player0PercentHP;
 let player1PercentHP;
 let player0PercentSP;
 let player1PercentSP;
-
 // declared variables for the boxes
 let gameBox;
 let headerBox;
@@ -29,12 +24,13 @@ let graphicsBox;
 let barsBox;
 let controlsBox;
 let outputBox;
-
+// creating a varible to show in the outputBox
+let currentText;
+let damageDone;
 class Fighter {
   constructor(name, charaName) {
     //'contructor' is in all JS classes
     // It gets run immediately when a new object is created from a class
-
     // Set all of our default values for this new fighter here
     this.name = name;
     this.hp = START_HP;
@@ -44,47 +40,34 @@ class Fighter {
     this.tek = DEFAULT_TEK;
     this.charaName = charaName;
   }
-
   //this logs who attacked who
   attack(target) {
-    console.log(this.name + ' attacked ' + target.name);
-    let damageRoll =
+    damageDone = Math.floor(Math.random() * 5) + 1;
+    console.log(koCheck(target, damageDone));
+    console.log(this.name + ' attacked ' + target.name + ' doing ' + damageDone + ' points of damage!');
+    console.log(target.name + ' has ' + target.hp + ' hp points left!');
   }
-
   single(target) {
     this.attack(target);
+    //creating text for outputBox
+      currentText = outputBox.innerHTML;
+      outputBox.innerHTML = this.name + ' attacked ' + target.name + ' doing ' + damageDone + ' points of damage! ' + target.name + ' has ' + target.hp + ' hp points left!';
+      outputBox.innerHTML += "<br>";
+      outputBox.innerHTML += currentText;
+    endTurn();
   }
-
   double(target) {
     this.attack(target);
     this.attack(target);
   }
-
   //this logs that they recovered
   recover() {
     console.log('Recovered!');
   }
 }
-
-function singleAttack() {
-  if(playerTurn == false) {
-  Player0.single(Player1);
-   playerTurn = true;
-   console.log('Player turn is now true');
-}else {
-  Player1.single(Player0);
-  playerTurn = false;
-  console.log('Player turn is now false');
-}
-}
-
-
-
-
 function startup() {
   Player0 = new Fighter(P0NAME, P0CHARA);
   Player1 = new Fighter(P1NAME, P1CHARA);
-
   //this makes a shortcut for 'document.getElementById'
   gameBox = document.getElementById('gameBox');
   headerBox = document.getElementById('headerBox');
@@ -92,20 +75,14 @@ function startup() {
   barsBox = document.getElementById('barsBox');
   controlsBox = document.getElementById('controlsBox');
   outputBox = document.getElementById('outputBox');
-
-
   //this shows the fighter images in the graphics box
   graphicsBox.innerHTML = '<img id ="' + Player0.charaName + '" src="img/' + Player0.charaName + '_idle.png" alt="' + Player0.name + '" class="fighterIMG">'
   graphicsBox.innerHTML += '<img id ="' + Player1.charaName + '" src="img/' + Player1.charaName + '_idle.png" alt="' + Player1.name + '" class="fighterIMG">'
-
-
   console.log("My name is " + Player0.name + " and my ATK is " + Player0.atk)
   console.log("My name is " + Player1.name + " and my ATK is " + Player1.atk)
-
   showControls() //runs the showControls() function
   updateBars() //runs the updateBars() function
 }
-
 function showControls() {
   //checks to see which players turn it is and show the apropriate controls
   if (playerTurn) {
@@ -120,20 +97,18 @@ function showControls() {
 function koCheck(target, amount) {
   target.hp = target.hp - amount;
   if (target.hp <= 0) {
+    hideControls();
     return true;
   } else {
     return false;
   }
 }
-
-
 function updateBars() {
   //calculates the percent of HP
   player0PercentHP = (Player0.hp / START_HP) * 100
   player1PercentHP = (Player1.hp / START_HP) * 100
   player0PercentHP = (Player0.sp / START_SP) * 100
   player1PercentHP = (Player1.sp / START_SP) * 100
-
   //Makes sure Player0's health is not greater than 100% or less than 0%
   if (player0PercentHP <= 0) {
     player0PercentHP = 0
@@ -142,7 +117,6 @@ function updateBars() {
   } else {
     player0PercentHP = player0PercentHP
   }
-
   //Makes sure Player1's health is not greater than 100% or less than 0%
   if (player1PercentHP <= 0) {
     player1PercentHP = 0
@@ -151,7 +125,6 @@ function updateBars() {
   } else {
     player1PercentHP = player1PercentHP
   }
-
   //Makes sure Player0's SP is not greater than 100% or less than 0%
   if (player0PercentSP <= 0) {
     player0PercentSP = 0
@@ -160,7 +133,6 @@ function updateBars() {
   } else {
     player0PercentSP = player0PercentSP
   }
-
   //Makes sure Player1's SP is not greater than 100% or less than 0%
   if (player1PercentSP <= 0) {
     player1PercentSP = 0
@@ -175,28 +147,19 @@ function updateBars() {
   barsBox.innerHTML += 'P1<div class="hpBar"><div style="height:' + player1PercentHP + '%; width: 100%;" id="p1HPfill" class="HPfill"></div></div>'
   barsBox.innerHTML += '<div class="spBar"><div style="height:' + player1PercentSP + '%; width: 100%;" id="p1SPfill" class="SPfill"></div></div>'
 }
-
 // EndTurn code
 function endTurn() {
   playerTurn = !playerTurn
-  if (kocheck(Player0, 0) || kocheck(Player1, 0)){
+  if (koCheck(Player0, 0) || koCheck(Player1, 0)){
     hideControls();
+  }else{
+    showControls();
   }
 }
-// this hides showControls when character is defeated
-function hideContols() {
+function hideControls() {
   controlsBox.innerHTML = "";
 }
-
-
-
-
-
-
 /*
-
 MHW = 'delicious'
-
 MHWoutput > MHWinput
-
 */
